35ada11499f1a671c439fba2fa9f471499ac6dcd
Taylor Scott <tayzscott@gmail.com>
2023-04-28 18:16:38 +0000

    clean-up and adding comments

--- Ex_Files/02_03: lists/main.py
+++ Ex_Files/02_03: lists/main.py
@@ -4,9 +4,9 @@ AGES = [20, 21, 22, 23]
 JOHN = NAMES[0]
 PAUL = NAMES[1]
 
-JOHN_PAUL = NAMES[:2]
+JOHN_PAUL = NAMES[:2]  # every name left of index 2
 GEORGE_RINGO = NAMES[2:]
-REVERSE = NAMES[::-1]
+REVERSE = NAMES[::-1]  # start:end:step
 EVERY_OTHER = NAMES[::2]
 
 print(sum(AGES))
